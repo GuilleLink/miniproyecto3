6e468f7e2f65fcd064889382e7e3feaac3e8ad60
GuilleLink <32148569+GuilleLink@users.noreply.github.com>
2020-09-27 14:50:51 -0600

    Init repo

---
+++ prueba1.py
@@ -0,0 +1,12 @@
+#Universidad del Valle de Guatemala
+#Modelacion y simulacion
+#Juan Guillermo Sandoval Lacayo - 17577
+#Hector Javier Carpio Garcia - 17077
+#Miniproyecto3
+
+#Imports
+
+
+#Code
+
+
