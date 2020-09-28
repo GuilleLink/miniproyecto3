e1296481577543202cb591104bc0490b9311d702
javiercarpio57 <javierrr.carpio57@gmail.com>
2020-09-27 18:39:19 -0600

    Task 2 completa y corrección de bug

--- utilities.py
+++ utilities.py
@@ -5,7 +5,7 @@
 #Miniproyecto3
 
 #Imports
-import numpy
+import numpy as np
 import random
 import math
 
@@ -14,58 +14,60 @@ def simulacion(T, lambda_max, solicitudes, servidores):
     #Tiempo inicial
     t = 0
     
-    u = random.random()
-    ta = t-((math.log(u)/lambda_max))
-
-    td = numpy.zeros(servidores) + numpy.infty
-    A = {}
-    D = {}
-
-
     #Numero de clientes
     n = 0 
-    #Numero de cliente en el servidor (0 libre)
-    ni = numpy.zeros(servidores)
+
     #Numero de entradas
     Na = 0
     #Numero de salidas
-    Nd = numpy.zeros(servidores)
+    Nd = np.zeros(servidores)
+    #Tiempos ocupado de cada servidor
+    NTD = np.zeros(servidores)
+
+    A = {}
+    D = {}
     #Numero de tiempos de ocurrencia
     NTs = []
-    #Tiempos ocupado de cada servidor
-    NTD = numpy.zeros(servidores)
+
+    u = np.random.uniform()
+    ta = t-((np.log(u)/lambda_max))
+
+    td = np.zeros(servidores) + np.infty
+    #Numero de cliente en el servidor (0 libre)
+    ni = np.zeros(servidores)
 
     while t < T:
-        # print(ta, td, min(ta, min(td)))
         if ta == min(ta, min(td)):
             t = ta
             Na += 1
-            u = random.random()
-            ta = t-((math.log(u)/lambda_max))
+
+            u = np.random.uniform()
+            ta = t - ((np.log(u)/lambda_max))
+            
             A[Na] = t
             if n < servidores:
-                for i in range(0, servidores):
-                    if (ni[i] == 0):
+                for i in range(servidores):
+                    if ni[i] == 0:
                         ni[i] = Na
-                        NTs.append(t-A[Na])
-                        td[i] = t + numpy.random.exponential(solicitudes)
-                        NTD[i] += td[i]-t
+                        NTs.append(t - A[Na])
+                        td[i] = t + np.random.exponential(solicitudes)
+                        NTD[i] += td[i] - t
                         break
             n += 1
         else:
             #Next TimeStamp
-            nts = numpy.argmin(td)
+            nts = np.argmin(td)
             t = td[nts]
             Nd[nts] += 1
             D[ni[nts]] = t
-            if (n <= servidores):
+            if n <= servidores:
                 ni[nts] = 0
-                td[nts] = numpy.infty
+                td[nts] = np.infty
             else:
                 index = max(ni) + 1
                 ni[nts] = index
-                NTs.append(t - A[index])
-                td[nts] = t + numpy.random.exponential(solicitudes)
+                NTs.append(t - A[Na])
+                td[nts] = t + np.random.exponential(solicitudes)
                 NTD[nts] += td[nts] - t
 
             n -= 1
